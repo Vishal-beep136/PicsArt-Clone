dccb90cfdc17e0e6d68ff0c25176bdcbe9ec7893
Vishal Beep <alphatech@gmail.com>
2021-06-05 10:30:34 +0530

    hello world

---
+++ app/src/main/java/kaitka/vishal/meeta/bigart/ResultActivity.java
@@ -0,0 +1,22 @@
+package kaitka.vishal.meeta.bigart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kaitka.vishal.meeta.bigart.databinding.ActivityResultBinding;
+
+public class ResultActivity extends AppCompatActivity {
+
+    ActivityResultBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityResultBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+
+        binding.image.setImageURI(getIntent().getData());
+    }
+}
